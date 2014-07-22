0c2bbc7fce9deac1987261811652274ec3979bdb
Mimi Yin <mimi@parlor.is>
2014-07-22 00:51:53 -0400

    static

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import os
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 # Django settings for Prosaic project.
 
 DEBUG = True
@@ -56,7 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_PATH + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '/static/'
 )
 
 # List of finder classes that know how to find static files in
@@ -113,7 +117,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'prosaic.urls'
 
 TEMPLATE_DIRS = (
-    '/home/capps/webapps/django_prosaic/prosaic/templates'
+    PROJECT_PATH + '/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -128,7 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'prosaic',
-    'prosaic.words',
+    'words',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -169,4 +173,4 @@ CACHES = {
     }
 }
 
-#from settings_local import *
+from settings_local import *
